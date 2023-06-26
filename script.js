10446b881020c486dfb943b2dbce6264faba5a70
andrewwmcgrath <137793738+andrewwmcgrath@users.noreply.github.com>
2023-06-26 14:26:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+document.getElementById("myForm").addEventListener("submit", function(event){
+  event.preventDefault();
 
+  // Get form data
+  let formData = new FormData(event.target);
+  
+  // Collect answers
+  let answers = [];
+  for(let [name, value] of formData){
+    answers.push(`${name} = ${value}`);
+  }
+  
+  // Show user their answers
+  alert(`You answered:\n${answers.join('\n')}`);
+});
