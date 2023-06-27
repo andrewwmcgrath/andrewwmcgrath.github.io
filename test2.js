337f71567adb15b6442297b55f07874e3815bec1
andrewwmcgrath <137793738+andrewwmcgrath@users.noreply.github.com>
2023-06-27 14:01:28 +0100

    Update test2.js

--- test2.js
+++ test2.js
@@ -3,15 +3,9 @@ let questions = [
     {text: 'Question 1', description: 'This is a description for question 1'},
     {text: 'Question 2', description: 'This is a description for question 2'},
     {text: 'Question 3', description: 'This is a description for question 3'},
+    {text: 'Ques', description: 'This is a  for question 4'},
+    {text: 'Question 5', description: 'This is a description for question 5'},
 ];
-
-// Define your previous cycle data
-let prevCycleData = [
-    {prevValue: 3, avgValue: 4},
-    {prevValue: 3, avgValue: 4},
-    {prevValue: 3, avgValue: 4},
-];
-
 // Get the form container
 let form = document.getElementById("myForm");
 
@@ -45,16 +39,18 @@ questions.forEach((questionObj, index) => {
             select.appendChild(option);
         }
         form.appendChild(select);
-
-        // Display the previous cycle's data
-        let prevDataP = document.createElement("p");
-        prevDataP.innerText = `Previous value: ${prevCycleData[index].prevValue}, Average value: ${prevCycleData[index].avgValue}`;
-        form.appendChild(prevDataP);
     });
 
     // Add a line break for readability
     form.appendChild(document.createElement("br"));
 });
+
+// Assuming previous data 
+let prevCycleData = [
+    {prevValue: 3, avgValue: 4},
+    {prevValue: 3, avgValue: 4},
+    {prevValue: 3, avgValue: 4},
+];
 
 function submitForm() {
     let selectElements = form.getElementsByTagName('select');
