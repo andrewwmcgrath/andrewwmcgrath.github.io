6097acb3140473cde54e3105c8ea4179602c89e8
andrewwmcgrath <137793738+andrewwmcgrath@users.noreply.github.com>
2023-06-27 12:46:43 +0100

    Update script2.js

--- script2.js
+++ script2.js
@@ -17,7 +17,61 @@ let form = document.getElementById("myForm");
 
 // Iterate over each question
 questions.forEach((questionObj, index) => {
-    // Here, you would create elements for each question, description, 
-    // dropdown, and the previous cycle's data.
-    // ...
+    // Create question header
+    let header = document.createElement("h2");
+    header.innerText = questionObj.text;
+    form.appendChild(header);
+
+    let desc = document.createElement("p");
+    desc.innerText = questionObj.description;
+    form.appendChild(desc);
+
+    // Create select fields for Probability, Impact and Preparation
+    ['Probability', 'Impact', 'Preparation'].forEach(choice => {
+        // Create label
+        let label = document.createElement("label");
+        label.setAttribute("for", `question${index + 1}${choice}`);
+        label.innerText = `${choice}:`;
+        form.appendChild(label);
+
+        // Create select
+        let select = document.createElement("select");
+        select.setAttribute("id", `question${index + 1}${choice}`);
+        select.setAttribute("name", `question${index + 1}${choice}`);
+        for(let i = 1; i <= 6; i++) {
+            let option = document.createElement("option");
+            option.setAttribute("value", i);
+            option.innerText = i;
+            select.appendChild(option);
+        }
+        form.appendChild(select);
+
+        // Display the previous cycle's data
+        let prevDataP = document.createElement("p");
+        prevDataP.innerText = `Previous value: ${prevCycleData[index].prevValue}, Average value: ${prevCycleData[index].avgValue}`;
+        form.appendChild(prevDataP);
+    });
+
+    // Add a line break for readability
+    form.appendChild(document.createElement("br"));
 });
+
+function submitForm() {
+    let selectElements = form.getElementsByTagName('select');
+    let participantId = document.getElementById('participantId').value;
+    let formData = new FormData();
+
+    formData.append('participantId', participantId);
+    for(let i = 0; i < selectElements.length; i++) {
+        formData.append(selectElements[i].name, selectElements[i].value);
+    }
+
+    // Collect answers
+    let answers = [];
+    for(let [name, value] of formData){
+        answers.push(`${name} = ${value}`);
+    }
+    
+    // Show user their answers
+    alert(`You answered:\n${answers.join('\n')}`);
+}
