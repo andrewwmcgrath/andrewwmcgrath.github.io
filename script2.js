fe10314ba7a9aa66e68868e39d9fbb218dd885b7
andrewwmcgrath <137793738+andrewwmcgrath@users.noreply.github.com>
2023-06-27 12:26:51 +0100

    Create script2.js

---
+++ script2.js
@@ -0,0 +1,23 @@
+// Define your questions
+let questions = [
+    {text: 'Question 1', description: 'This is a description for question 1'},
+    {text: 'Question 2', description: 'This is a description for question 2'},
+    {text: 'Question 3', description: 'This is a description for question 3'},
+];
+
+// Define your previous cycle data
+let prevCycleData = [
+    {prevValue: 3, avgValue: 4},
+    {prevValue: 3, avgValue: 4},
+    {prevValue: 3, avgValue: 4},
+];
+
+// Get the form container
+let form = document.getElementById("myForm");
+
+// Iterate over each question
+questions.forEach((questionObj, index) => {
+    // Here, you would create elements for each question, description, 
+    // dropdown, and the previous cycle's data.
+    // ...
+});
